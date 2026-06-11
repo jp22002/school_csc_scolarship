376cb23a8d6818ea8d705f3e265cdf1a0d71c09a
jp22002 <jp22002@students.sacredheart.school.nz>
2026-06-11 14:51:08 +1200

    better

--- code2105/code.py
+++ code2105/code.py
@@ -1,11 +1,11 @@
 def print_names(people):
     people_place = 0
     while people_place != (len(people)):
-        to_print = ""
-        name_place = 0
+        to_print = people[people_place][0]
+        name_place = 1
         while name_place != (len(people[people_place])):
             name = people[people_place][name_place]
-            to_print += name + " "
+            to_print +=  " " +name 
             name_place += 1
         print(to_print)
         people_place += 1
