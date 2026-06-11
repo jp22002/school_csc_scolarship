76607dc4db772281f6c0699f4baff157c903e21a
jp22002 <jp22002@students.sacredheart.school.nz>
2026-06-11 14:46:49 +1200

    make better

--- code2105/code2.py
+++ code2105/code2.py
@@ -1,13 +1,13 @@
 def run_length_encode(data):
     dicti = {}
     for i in data:
-        if i in dicti:
+        if i in dicti.keys():
             dicti[i] += 1
         else:
             dicti[i] = 1
-    final = ()
+    final = []
     for i in dicti:
-        final += ((i, dicti[i]),)
+        final.append((i, dicti[i]))
     return(final)
 
 data = [10,20,30,30,30,20]
